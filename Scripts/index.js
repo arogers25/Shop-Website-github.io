332e7957c3cbb1814ff1a1b45d781d4e20ff1175
Andreas Rogers <dreandreas@outlook.com>
2022-03-10 14:53:05 -0700

    Update

---
+++ Scripts/index.js
@@ -0,0 +1,46 @@
+//alert("You made it to index.html!");
+let numItems = 0;
+if (localStorage.getItem("totalItems") == null) localStorage.setItem("totalItems", 0);
+numItems = parseInt(localStorage.getItem("totalItems"));
+
+let totalPriceVar = 0;
+if (localStorage.getItem("totalPrice") == null) localStorage.setItem("totalPrice", 0);
+totalPriceVar = parseInt(localStorage.getItem("totalPrice"));
+
+document.getElementById('itemsAmount').innerHTML = numItems;
+function isValid(number)
+{
+  if (number == "")
+  {
+    alert("Please enter an amount");
+    return false;
+  }
+  if (number <= 0)
+  {
+    alert("Please enter an amount greater than 0");
+    return false;
+  }
+  if (isNaN(number))
+  {
+    alert("Please enter a valid amount");
+    return false;
+  }
+  return true;
+}
+
+function addToCart(amount, price)
+{
+  if (isValid(amount))
+  {
+    numItems += parseInt(amount);
+    localStorage.setItem("totalItems", numItems);
+    document.getElementById('itemsAmount').innerHTML = numItems;
+  }
+}
+
+function resetItems()
+{
+  numItems = 0;
+  localStorage.setItem("totalItems", numItems);
+  document.getElementById('itemsAmount').innerHTML = numItems;
+}
